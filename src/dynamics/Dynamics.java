170f5a00efbb9e3f11f251a3c0f41004ad1404aa
Daxillion <beonicwulf@yandex.ru>
2026-03-19 13:45:22 +0300

    Setting up

---
+++ src/dynamics/Dynamics.java
@@ -0,0 +1,11 @@
+package dynamics;
+
+import mindustry.*;
+import mindustry.mod.*;
+import mindustry.type.*;
+
+import static arc.Core.*;
+
+public class Dynamics extends Mod{
+
+}
---
+++ src/dynamics/content/DynamicsItems.java
@@ -0,0 +1,4 @@
+package dynamics.content;
+
+public class DynamicsItems {
+}
