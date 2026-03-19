8e3a752ae6deedc664d0e7b5945b1d035aebd348
Daxillion <beonicwulf@yandex.ru>
2026-03-19 13:32:15 +0300

    Setting up

--- src/dynamics/MainCD.java
+++ src/dynamics/MainCD.java
@@ -1,17 +1,14 @@
-package example;
+package dynamics;
 
 import arc.*;
 import arc.util.*;
-import mindustry.*;
-import mindustry.content.*;
 import mindustry.game.EventType.*;
-import mindustry.gen.*;
 import mindustry.mod.*;
 import mindustry.ui.dialogs.*;
 
-public class ExampleJavaMod extends Mod{
+public class MainCD extends Mod{
 
-    public ExampleJavaMod(){
+    public MainCD(){
         Log.info("Loaded ExampleJavaMod constructor.");
 
         //listen for game load event
