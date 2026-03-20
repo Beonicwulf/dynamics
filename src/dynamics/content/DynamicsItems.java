5fe54ad6644dcc48a4358bf4c48a18b8dcc3fba0
Daxillion <beonicwulf@yandex.ru>
2026-03-20 09:53:27 +0300

    Added Basic Component

--- src/dynamics/content/DynamicsItems.java
+++ src/dynamics/content/DynamicsItems.java
@@ -8,23 +8,29 @@ import mindustry.type.*;
 public class DynamicsItems {
     public static Item
             /* Resources */
-            zinc, tantalum
+            zinc, tantalum, component
             ;
 
     public static final Seq<Item> dynamicsItems = new Seq<>();
 
     public static void load() {
         zinc = new Item("zinc", Color.valueOf("#b09f74")) {{
-            cost = 2;
+            cost = 1;
             hardness = 1;
+            alwaysUnlocked = true;
+            healthScaling = 0.1f;
         }};
         tantalum = new Item("tantalum", Color.valueOf("#606d90")) {{
-            cost = 3;
+            cost = 1.5f;
             hardness = 4;
+            healthScaling = 0.6f;
+        }};
+        component = new Item("component", Color.valueOf("#b09f74")) {{
+            cost = 2;
         }};
 
         dynamicsItems.addAll(
-                zinc, tantalum
+                zinc, tantalum, component
         );
     }
 }
