f5f13ab734e5c9b363daa37fa898e8f2cf9f2bf5
Daxillion <beonicwulf@yandex.ru>
2026-03-20 12:01:43 +0300

    Added Mechanical Press

--- src/dynamics/Dynamics.java
+++ src/dynamics/Dynamics.java
@@ -1,6 +1,8 @@
 package dynamics;
 
+import dynamics.content.DynamicsBlocks;
 import dynamics.content.DynamicsItems;
+import dynamics.content.DynamicsLiquids;
 import mindustry.mod.*;
 
 public class Dynamics extends Mod{
@@ -8,5 +10,7 @@ public class Dynamics extends Mod{
     @Override
     public void loadContent() {
         DynamicsItems.load();
+        DynamicsLiquids.load();
+        DynamicsBlocks.load();
     }
 }
---
+++ src/dynamics/content/DynamicsBlocks.java
@@ -0,0 +1,43 @@
+package dynamics.content;
+
+import arc.math.Mathf;
+import mindustry.type.Category;
+import mindustry.type.ItemStack;
+import mindustry.world.Block;
+import mindustry.world.blocks.production.*;
+import mindustry.world.draw.*;
+
+import static mindustry.type.ItemStack.with;
+
+public class DynamicsBlocks {
+    public static Block
+            //crafting
+            mechanicalPress
+            ;
+
+    public static void load() {
+        //crafting
+        mechanicalPress = new GenericCrafter("mechanical-press"){{
+            requirements(Category.crafting, with(DynamicsItems.zinc, 80));
+
+            outputItem = new ItemStack(DynamicsItems.component, 1);
+            craftTime = 90f;
+            size = 2;
+            hasItems = true;
+
+            consumeItem(DynamicsItems.zinc, 3);
+            consumeLiquid(DynamicsLiquids.steam, 5f / 60f);
+
+            drawer = new DrawMulti(new DrawRegion("-bottom"),
+                    new DrawPistons(){{
+                        sinMag = 2.75f;
+                        sinScl = 5f;
+                        sides = 8;
+                        sideOffset = Mathf.PI / 2f;
+                    }},
+                    new DrawRegion("-mid"),
+                    new DrawRegion("-top")
+            );
+        }};
+    }
+}
