d3ec074450c69bce37dc3460a6985c4af38f9ece
Beonicwulf <beonicwulf@yandex.ru>
2026-03-22 22:26:10 +0300

    Update DynamicsRecipes.java

--- src/dynamics/content/DynamicsRecipes.java
+++ src/dynamics/content/DynamicsRecipes.java
@@ -8,15 +8,11 @@ import static mindustry.content.Liquids.ozone;
 
 public class DynamicsRecipes {
     public static Seq<Recipe>
-    basicComponentRecipe, testRecipe
+    basicComponentRecipe
             ;
     public static void load() {
         basicComponentRecipe = Seq.with(
-                new Recipe(120f).consumeItems(ItemStack.with(DynamicsItems.malachite, 5)).outputItem(DynamicsItems.component)
-        );
-        testRecipe = Seq.with(
-                new Recipe(120f).consumeItems(ItemStack.with(DynamicsItems.malachite, 5)).outputItem(DynamicsItems.component),
-                new Recipe(60f).consumeItems(ItemStack.with(DynamicsItems.tantalum, 5)).consumeLiquid(ozone, 10f/60f).outputItem(DynamicsItems.sublime, 2)
+                new Recipe(120f).consumeItems(ItemStack.with(DynamicsItems.zinc, 10, DynamicsItems.malachite, 5)).outputItem(DynamicsItems.component)
         );
     }
 }
