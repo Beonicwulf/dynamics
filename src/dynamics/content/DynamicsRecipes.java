2c2301685bd0765e0249b4ac09f730ddc64a5ea2
Beonicwulf <beonicwulf@yandex.ru>
2026-03-22 22:00:48 +0300

    Update DynamicsRecipes.java

--- src/dynamics/content/DynamicsRecipes.java
+++ src/dynamics/content/DynamicsRecipes.java
@@ -12,11 +12,11 @@ public class DynamicsRecipes {
             ;
     public static void load() {
         basicComponentRecipe = Seq.with(
-                new Recipe(120f).consumeItems(ItemStack.with(DynamicsItems.zinc, 10, DynamicsItems.malachite, 5)).consumeLiquid(DynamicsLiquids.steam, 12f/60f).outputItem(DynamicsItems.component)
+                new Recipe(120f).consumeItems(ItemStack.with(DynamicsItems.malachite, 5)).outputItem(DynamicsItems.component)
         );
         testRecipe = Seq.with(
-                new Recipe(120f).consumeItems(ItemStack.with(DynamicsItems.zinc, 10, DynamicsItems.malachite, 5)).consumeLiquid(DynamicsLiquids.steam, 12f/60f).outputItem(DynamicsItems.component),
-                new Recipe(60f).consumeItems(ItemStack.with(DynamicsItems.zinc, 5)).consumeLiquid(ozone, 10f/60f).outputItem(DynamicsItems.sublime, 2)
+                new Recipe(120f).consumeItems(ItemStack.with(DynamicsItems.malachite, 5)).outputItem(DynamicsItems.component),
+                new Recipe(60f).consumeItems(ItemStack.with(DynamicsItems.tantalum, 5)).consumeLiquid(ozone, 10f/60f).outputItem(DynamicsItems.sublime, 2)
         );
     }
 }
