c1bd67bf130128b27e443d400f0cec8a5c907726
Beonicwulf <beonicwulf@yandex.ru>
2026-03-22 22:55:32 +0300

    Update DynamicsRecipes.java

--- src/dynamics/content/DynamicsRecipes.java
+++ src/dynamics/content/DynamicsRecipes.java
@@ -4,16 +4,14 @@ import arc.struct.Seq;
 import dynamics.world.blocks.crafting.Recipe;
 import mindustry.type.ItemStack;
 
-import static mindustry.content.Liquids.ozone;
-
 public class DynamicsRecipes {
     public static Seq<Recipe>
     basicComponentRecipe
             ;
     public static void load() {
         basicComponentRecipe = Seq.with(
-                new Recipe(120f).consumeItems(ItemStack.with(DynamicsItems.zinc, 10, DynamicsItems.malachite, 5)).outputItem(DynamicsItems.component),
-                new Recipe(60f).consumeItems(ItemStack.with(DynamicsItems.tantalum, 5, DynamicsItems.zinc, 5)).outputItem(DynamicsItems.sublime)
+                new Recipe(120f).consumeItems(ItemStack.with(DynamicsItems.malachite, 5, DynamicsItems.zinc, 10)).outputItem(DynamicsItems.component),
+                //new Recipe(60f).consumeItems(ItemStack.with(DynamicsItems.tantalum, 5, DynamicsItems.zinc, 10)).outputItem(DynamicsItems.sublime)
         );
     }
 }
