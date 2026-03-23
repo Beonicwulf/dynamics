86b6132e598468c792adbcbd107e4b49e36507c9
Daxillion <beonicwulf@yandex.ru>
2026-03-23 09:48:05 +0300

    Added Clockwork Drill

--- src/dynamics/content/DynamicsBlocks.java
+++ src/dynamics/content/DynamicsBlocks.java
@@ -3,11 +3,13 @@ package dynamics.content;
 import dynamics.content.blocks.DynamicsCrafting;
 import dynamics.content.blocks.DynamicsDistribution;
 import dynamics.content.blocks.DynamicsLiquidBlocks;
+import dynamics.content.blocks.DynamicsProduction;
 
 public class DynamicsBlocks {
     public static void load() {
         DynamicsCrafting.load();
         DynamicsDistribution.load();
         DynamicsLiquidBlocks.load();
+        DynamicsProduction.load();
     }
 }
---
+++ src/dynamics/content/blocks/DynamicsProduction.java
@@ -0,0 +1,33 @@
+package dynamics.content.blocks;
+
+import dynamics.content.DynamicsItems;
+import dynamics.content.DynamicsLiquids;
+import mindustry.content.Fx;
+import mindustry.entities.effect.MultiEffect;
+import mindustry.graphics.Pal;
+import mindustry.type.Category;
+import mindustry.world.Block;
+import mindustry.world.blocks.production.BurstDrill;
+import mindustry.world.draw.*;
+
+import static mindustry.type.ItemStack.with;
+
+public class DynamicsProduction {
+    public static Block
+    clockworkDrill
+    ;
+
+    public static void load() {
+        clockworkDrill = new BurstDrill("clockwork-drill") {{
+            requirements(Category.production, with(DynamicsItems.zinc, 50, DynamicsItems.partBasic, 5));
+            size = 3;
+            drillTime = 60f * 6f;
+            shake = 2f;
+            drillEffect = new MultiEffect(Fx.mineImpact, Fx.drillSteam, Fx.mineImpactWave.wrap(Pal.redLight, 40f));
+            consumeLiquid(DynamicsLiquids.steam, 12f / 60f);
+            hasPower = false;
+            tier = 2;
+            drillMultipliers.put(DynamicsItems.zinc, 2f);
+        }};
+    }
+}
