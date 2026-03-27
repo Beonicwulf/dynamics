78d9bf6a4d67335f8cddb1c7d5c4bd8900f3d602
Daxillion <beonicwulf@yandex.ru>
2026-03-27 09:34:40 +0300

    Pipe Tunnel added

--- src/dynamics/content/blocks/DynamicsLiquidBlocks.java
+++ src/dynamics/content/blocks/DynamicsLiquidBlocks.java
@@ -3,13 +3,14 @@ package dynamics.content.blocks;
 import dynamics.content.DynamicsItems;
 import mindustry.type.Category;
 import mindustry.world.Block;
+import mindustry.world.blocks.distribution.DirectionLiquidBridge;
 import mindustry.world.blocks.liquid.*;
 
 import static mindustry.type.ItemStack.*;
 
 public class DynamicsLiquidBlocks {
     public static Block
-            pipe, pipeRouter
+            pipe, pipeRouter, pipeTunnel
             ;
 
     public static void load() {
@@ -17,12 +18,29 @@ public class DynamicsLiquidBlocks {
             requirements(Category.liquid, with(DynamicsItems.zinc, 2));
             leaks = true;
             underBullets = true;
-
+            placeableLiquid = true;
+            //explosivenessScale = flammabilityScale = 20f/50f;
         }};
         pipeRouter = new LiquidRouter("pipe-router") {{
             requirements(Category.liquid, with(DynamicsItems.zinc, 10, DynamicsItems.partBasic, 1));
             liquidCapacity = 40;
             size = 1;
+            placeableLiquid = true;
+            liquidPadding = 3f/4f;
+            researchCostMultiplier = 3;
+            underBullets = true;
+            solid = false;
+            //explosivenessScale = flammabilityScale = 40f/150f;
+        }};
+        pipeTunnel = new DirectionLiquidBridge("pipe-tunnel") {{
+            requirements(Category.liquid, with(DynamicsItems.zinc, 20,DynamicsItems.partBasic, 5));
+            solid = false;
+            range = 8;
+            hasPower = false;
+            liquidCapacity = 60f;
+            underBullets = true;
+            //explosivenessScale = flammabilityScale = 20f/120f;
+            ((Conduit)pipe).rotBridgeReplacement = this;
         }};
     }
 }
