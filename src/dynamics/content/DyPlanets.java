a3aeaac7100370ae384e6220cfd10e787544f614
Beonicwulf <beonicwulf@yandex.ru>
2026-09-05 11:52:26 +0300

    Update DyPlanets.java

--- src/dynamics/content/DyPlanets.java
+++ src/dynamics/content/DyPlanets.java
@@ -157,12 +157,12 @@ public class DyPlanets {
             ruleSetter = r -> {
                 r.waveTeam = DyTeams.dread;
                 r.placeRangeCheck = false;
-                r.showSpawns = true;
+                r.hideSpawns = true;
                 r.coreIncinerates = true;
                 r.onlyDepositCore = false;
             };
             campaignRuleDefaults.fog = false;
-            
+            campaignRuleDefaults.hideSpawns = true;
             campaignRuleDefaults.rtsAI = campaignRuleDefaults.randomWaveAI = false;
 
             unlockedOnLand.add(DyEffectBlocks.coreSurface);
